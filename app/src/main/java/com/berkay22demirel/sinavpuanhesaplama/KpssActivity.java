1e7b2db3132b51442648926d4f29fffce6a4f3c2
berkay <berkay_demirel22@hotmail.com>
2019-05-19 12:15:56 +0300

    add kpss page

--- app/src/main/java/com/berkay22demirel/sinavpuanhesaplama/KpssActivity.java
+++ app/src/main/java/com/berkay22demirel/sinavpuanhesaplama/KpssActivity.java
@@ -1,13 +1,59 @@
 package com.berkay22demirel.sinavpuanhesaplama;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.berkay22demirel.sinavpuanhesaplama.Enum.ExamsEnum;
+import com.berkay22demirel.sinavpuanhesaplama.Util.CommonUtil;
 
 public class KpssActivity extends AppCompatActivity {
+
+    private static String PAGE_TITLE = ExamsEnum.KPSS.getTitle();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kpss);
+        getSupportActionBar().setTitle(CommonUtil.getPageTitle(PAGE_TITLE));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.exam_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+            case R.id.action_settings:
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+            case R.id.action_alert:
+                showAlertDialog();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showAlertDialog() {
+        new AlertDialog.Builder(KpssActivity.this)
+                .setTitle("UYARI!")
+                .setMessage("*Bu sayfa yapım aşamasındadır!")
+                .setPositiveButton(getResources().getText(R.string.button_ok), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).setIcon(getResources().getDrawable(R.drawable.alert_dialog_icon))
+                .show();
     }
 }
