c6e216e0a52bd0231debbd9cb354b45eff98604a
berkay <berkay_demirel22@hotmail.com>
2019-05-23 01:57:28 +0300

    fixed bug

--- app/src/main/java/com/berkay22demirel/sinavpuanhesaplama/Util/AdUtil.java
+++ app/src/main/java/com/berkay22demirel/sinavpuanhesaplama/Util/AdUtil.java
@@ -6,10 +6,6 @@ import com.google.android.gms.ads.AdView;
 
 public class AdUtil {
 
-    public static final String APPLICATION_ID = "ca-app-pub-3727603707347204~7305031067";
-    public static final String MAIN_BANNER_ID = "ca-app-pub-3727603707347204/1861132690";
-    public static final String EXAM_BANNER_ID = "";
-
     public static void showAd (AdView mAdView) {
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
